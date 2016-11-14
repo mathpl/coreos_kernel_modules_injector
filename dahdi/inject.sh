0f4f752dbdd74c7dbe0cd03e854cb973f066a088
Mathieu Payeur Levallois <mpl@jive.com>
2016-11-14 18:35:06 -0500

    better module list

--- dahdi/inject.sh
+++ dahdi/inject.sh
@@ -3,7 +3,7 @@
 # sudo docker run -ti --privileged jcr.io/coreos/dahdi-injector:1185.3.0-2.10.2 inject
 
 depmod
-for m in "dahdi_transcode dahdi_voicebus dahdi crc_ccitt xpp wcb4xxp wctdm wcfxo wctdm24xxp wcte11xp wct1xxp wcte12xp wct4xxp"
+for m in dahdi_transcode dahdi_voicebus dahdi crc_ccitt xpp wcb4xxp wctdm wcfxo wctdm24xxp wcte11xp wct1xxp wcte12xp wct4xxp
 do
     modprobe $m
 done
