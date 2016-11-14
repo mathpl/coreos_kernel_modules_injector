6d65c55fbed318f12cc064067629a95a37c55b44
Mathieu Payeur Levallois <mpl@jive.com>
2016-11-14 17:46:42 -0500

    fix on path

--- dahdi/inject.sh
+++ dahdi/inject.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# sudo docker run -ti --privileged -v=/usr/share/oem/:/target/oem -v=/opt:/target/opt jcr.io/coreos/zfs-injector:1185.3.0-0.6.5.8-0.6.5.8 inject
+# sudo docker run -ti --privileged jcr.io/coreos/dahdi-injector:1185.3.0-2.10.2 inject
 
 depmod
-modprobe dahdi
+for m in "dahdi_transcode dahdi_voicebus dahdi crc_ccitt xpp wcb4xxp wctdm wcfxo wctdm24xxp wcte11xp wct1xxp wcte12xp wct4xxp"
+do
+    modprobe $m
+done
