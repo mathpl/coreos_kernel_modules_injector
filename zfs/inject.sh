a173b56eb04f63c97e05a31559a768670965a53e
Mathieu Payeur Levallois <mpl@jive.com>
2017-01-04 01:32:47 -0500

    missing star

--- zfs/inject.sh
+++ zfs/inject.sh
@@ -5,7 +5,7 @@
 mkdir /target/bin /target/sbin /target/lib64 2>/dev/null
 cp -a /usr/local/bin/* /target/bin/
 cp -a /usr/local/sbin/* /target/sbin/
-cp -a /usr/local/lib64/ /target/lib64
+cp -a /usr/local/lib64/* /target/lib64
 
 depmod
 modprobe zfs
