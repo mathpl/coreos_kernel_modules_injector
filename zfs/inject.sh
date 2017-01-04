c5003cfb4252188d4639cad244e8a9e1f77d5599
Mathieu Payeur Levallois <mpl@jive.com>
2017-01-04 01:13:20 -0500

    copy from the right places

--- zfs/inject.sh
+++ zfs/inject.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# sudo docker run -ti --privileged -v=/usr/share/oem/:/target/oem -v=/opt:/target/opt jcr.io/coreos/zfs-injector:1185.3.0-0.6.5.8-0.6.5.8 inject
+# sudo docker run -ti --privileged -v=/usr/share/oem/:/target/oem -v=/opt:/target/opt jcr.io/coreos/zfs-injector:1185.3.0-0.6.5.8-0.6.5.8 /inject.sh
 
-mkdir /target/opt/bin /target/opt/sbin 2>/dev/null
-cp -a /binaries/bin/* /target/opt/bin/
-cp -a /binaries/sbin/* /target/opt/sbin/
-cp -a /binaries/lib64/ /target/oem/
+mkdir /target/bin /target/sbin /target/lib64 2>/dev/null
+cp -a /usr/local/bin/* /target/bin/
+cp -a /usr/local/sbin/* /target/sbin/
+cp -a /usr/local/lib64/ /target/lib64
 
 depmod
 modprobe zfs
