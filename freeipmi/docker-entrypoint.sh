acb0127fa70d42ca4c8748101b0b54ff577e3d47
Thomas Boutry <thomas.boutry@logmein.com>
2019-01-21 16:07:38 -0500

    INFRA-2257 reduce amounth of files copied

--- freeipmi/docker-entrypoint.sh
+++ freeipmi/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -ex
 if [ "$1" = "install" ]; then
         cd /target
         ls /src/*.tbz2 | while read i; do
-                tar jxvf $i
+                tar -jxvf $i --exclude ./usr/include*
         done
 else
         exec "$@"
